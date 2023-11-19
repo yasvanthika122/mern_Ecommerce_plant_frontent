689f92aa2d9dc5d9ac7bc04af787959bb1b9ca9a
yasvanthika122 <118598932+yasvanthika122@users.noreply.github.com>
2023-11-19 15:10:00 +0530

    Update Banner.jsx

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -47,7 +47,7 @@ const responsive = {
 
 const Image = styled('img')(({ theme }) => ({
     width: '100%',
-    height: 230,
+    height: 310,
     [theme.breakpoints.down('sm')]: {
         objectFit: 'cover',
         height: 180,
